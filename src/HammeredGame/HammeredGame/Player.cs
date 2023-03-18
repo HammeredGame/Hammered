1fd8122d50bba25e72ec0af8d412af6182550152
Yuto Takano <takanoy@student.ethz.ch>
2023-03-18 23:39:29 +0100

    Determine player movement direction more accurately
    Use the camera facing direction (pos -> target vector) and project that
    onto the ground plane to get the forward direction. This is better than
    the previous approach of using the camera's view matrix to find the
    forward direction, as it leads to more accurate angles.

--- src/HammeredGame/HammeredGame/Player.cs
+++ src/HammeredGame/HammeredGame/Player.cs
@@ -2,10 +2,6 @@
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace HammeredGame
 {
@@ -44,25 +40,27 @@ namespace HammeredGame
             Vector3 oldPos = _characterPosition;
             bool moveDirty = false;
 
+            Vector3 forwardDirectionFromCamera = Vector3.Normalize(Vector3.Multiply(activeCamera.target - activeCamera.pos, new Vector3(1, 0, 1)));
+
             // Keyboard input
             if (inp.KeyDown(Keys.W))
             {
-                player_vel += new Vector3(activeCamera.view.Forward.X, 0, activeCamera.view.Forward.Z) * baseSpeed;
+                player_vel += forwardDirectionFromCamera;
                 moveDirty = true;
             }
             if (inp.KeyDown(Keys.S))
             {
-                player_vel += new Vector3(activeCamera.view.Backward.X, 0, activeCamera.view.Backward.Z) * baseSpeed;
+                player_vel += -forwardDirectionFromCamera;
                 moveDirty = true;
             }
             if (inp.KeyDown(Keys.A))
             {
-                player_vel += new Vector3(activeCamera.view.Left.X, 0, activeCamera.view.Left.Z) * baseSpeed;
+                player_vel += -Vector3.Cross(forwardDirectionFromCamera, Vector3.Down); // should be up, but the game is vertically flipped right now
                 moveDirty = true;
             }
             if (inp.KeyDown(Keys.D))
             {
-                player_vel += new Vector3(activeCamera.view.Right.X, 0, activeCamera.view.Right.Z) * baseSpeed;
+                player_vel += Vector3.Cross(forwardDirectionFromCamera, Vector3.Down);
                 moveDirty = true;
             }
 
@@ -76,20 +74,32 @@ namespace HammeredGame
                 if ((MovePad_UpDown < -Input.DEADZONE) || (MovePad_UpDown > Input.DEADZONE) || (MovePad_LeftRight < -Input.DEADZONE) || (MovePad_LeftRight > Input.DEADZONE))
                 {
                     player_vel.X = (MovePad_LeftRight * activeCamera.view.Right.X + MovePad_UpDown * activeCamera.view.Forward.X) * baseControllerSpeed; // left-right_control * right_from_camera + up-down_control * forward_from_camera
-                    player_vel.Z = (MovePad_LeftRight * activeCamera.view.Right.Z + MovePad_UpDown * activeCamera.view.Forward.Z) * baseControllerSpeed; // use this formala along x and z motions for character movement 
+                    player_vel.Z = (MovePad_LeftRight * activeCamera.view.Right.Z + MovePad_UpDown * activeCamera.view.Forward.Z) * baseControllerSpeed; // use this formala along x and z motions for character movement
                     moveDirty = true;
                 }
             }
-
-            _characterPosition += player_vel;
-            player_vel.X *= 0.5f;
-            player_vel.Z *= 0.5f;
-
             if (moveDirty)
             {
+
+                // Normalize to length 1 regardless of direction, so that diagonals aren't faster than straight
+                // Do this only within moveDirty, since otherwise player_vel can be 0 or uninitialised and its unit vector is NaN
+                player_vel.Normalize();
+                player_vel *= baseSpeed;
+
+                _characterPosition += player_vel;
+
+                // At this point, also rotate the player to the direction of movement
                 Vector3 lookDirection = _characterPosition - oldPos;
                 float angle = (float)Math.Atan2(lookDirection.X, lookDirection.Z);
                 _characterRotation = Quaternion.CreateFromAxisAngle(Vector3.UnitY, angle);
+            } else
+            {
+                // No new keypresses or controller interactions this round, so
+                // apply a gradual slowdown to any previous velocity
+                player_vel.X *= 0.5f;
+                player_vel.Z *= 0.5f;
+
+                _characterPosition += player_vel;
             }
 
             //// Mouse based rotation (leaving this here temporarily, probably won't need this)
