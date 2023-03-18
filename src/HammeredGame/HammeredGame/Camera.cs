80eb8933aea06a88326bc521dcd1f8e0fe22122d
Siddharth Menon <siddharth.krishnaleela@gmail.com>
2023-03-18 13:02:01 +0100

    Initial commit of code structure for Hammered

---
+++ src/HammeredGame/HammeredGame/Camera.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HammeredGame
+{
+    internal class Camera
+    {
+        public const float CAM_HEIGHT_OFFSET = 80f;
+
+        public const float FAR_PLANE = 500;
+        public Vector3 pos, target;                 // Camera position and target
+        public Matrix view, proj, view_proj;        // View Matrix, Projection Matrix
+        public Vector3 up;                          // Vector that points up
+        float current_angle;                        // player relative angle offset of camera (is this needed for us?)
+        float angle_velocity;                       // Speed of above rotation
+        float radius = 100.0f;                      // Distance of camera from player
+        Vector3 unit_direction;                     // direction of camera (normalized to distance of 1 unit)
+
+        Input inp;                                  // Input class for camera controls
+
+        private Vector3 _focusPoint = Vector3.Zero;
+        private Vector3 _cameraOffset_default = new Vector3(30f, -50f, -60f);
+        private Vector3 _cameraOffset_alt1 = new Vector3(-50f, -50f, -30f);
+        private Vector3 _cameraOffset_alt2 = new Vector3(-30f, -50f, 60f);
+        private Vector3 _cameraOffset_alt3 = new Vector3(50f, -50f, 30f);
+
+        public Camera(GraphicsDevice gpu, Vector3 upDirection, Input input)
+        {
+            up = upDirection;
+            pos = _cameraOffset_default;
+            target = _focusPoint;
+            view = Matrix.CreateLookAt(pos, target, up);
+            proj = Matrix.CreatePerspectiveFieldOfView(MathHelper.PiOver4, gpu.Viewport.AspectRatio, 0.1f, FAR_PLANE);
+            view_proj = view * proj;
+            inp = input;
+            unit_direction = view.Forward; unit_direction.Normalize();
+
+            //setCameraPosition(_cameraOffset_default);
+        }
+
+        // Temporary
+        public void MoveCamera(Vector3 move)
+        {
+            pos += move;
+            view = Matrix.CreateLookAt(pos, target, up);
+            view_proj = view * proj;
+        }
+
+        public void UpdateTarget(Vector3 newTarget)
+        {
+            target = newTarget; //target.Y -= 10;
+            view = Matrix.CreateLookAt(pos, target, up);
+            view_proj = view * proj;
+        }
+
+        // TEMPORARY - needs to be modified
+        public void UpdateCamera()
+        {
+            #region TEMPORARY_CAMERA_CONTROLS
+            if (inp.KeyDown(Keys.D1))
+            {
+                pos = _cameraOffset_default;
+            }
+            if (inp.KeyDown(Keys.D2))
+            {
+                pos = _cameraOffset_alt1;
+            }
+            if (inp.KeyDown(Keys.D3))
+            {
+                pos = _cameraOffset_alt2;
+            }
+            if (inp.KeyDown(Keys.D4))
+            {
+                pos = _cameraOffset_alt3;
+            }
+            #endregion
+
+            UpdateTarget(_focusPoint); // UpdateTarget(level.pos);
+        }
+    }
+}
---
+++ src/HammeredGame/HammeredGame/HammeredGame.cs
@@ -0,0 +1,140 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
+//using System.Numerics;
+
+namespace HammeredGame
+{
+    public class HammeredGame : Game
+    {
+        // DISPLAY
+        const int SCREENWIDTH = 1280;
+        const int SCREENHEIGHT = 720;
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+        private GraphicsDevice _device;
+        private GraphicsDevice gpu;
+        private Effect _effect;
+        static public int screenW, screenH;
+        Camera _camera;
+
+        // INPUT and other related stuff
+        Input inp;
+
+        // RENDER TARGETS & TEXTURES
+        RenderTarget2D MainTarget;
+        //Texture2D test_tex;
+
+        // RECTANGLES WOOOO
+        Rectangle desktopRect;
+        Rectangle screenRect;
+
+        // 3D Objects and other related stuff
+        private Player _player;
+        private Texture2D playerTex;
+        private WorldObject _ground;
+        private List<GameObject> gameObjects;
+
+
+        public HammeredGame()
+        {
+            int desktop_width = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width - 10;
+            int desktop_height = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height - 10;
+            _graphics = new GraphicsDeviceManager(this)
+            {
+                PreferredBackBufferWidth = desktop_width,
+                PreferredBackBufferHeight = desktop_height,
+                IsFullScreen = false,
+                PreferredDepthStencilFormat = DepthFormat.None,
+                GraphicsProfile = GraphicsProfile.HiDef
+            };
+            Window.IsBorderless = true;
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+            gpu = GraphicsDevice;
+            PresentationParameters pp = gpu.PresentationParameters;
+            _spriteBatch = new SpriteBatch(gpu);
+            MainTarget = new RenderTarget2D(gpu, SCREENWIDTH, SCREENHEIGHT, false, pp.BackBufferFormat, DepthFormat.Depth24);
+            screenW = MainTarget.Width;
+            screenH = MainTarget.Height;
+            desktopRect = new Rectangle(0, 0, pp.BackBufferWidth, pp.BackBufferHeight);
+            screenRect = new Rectangle(0, 0, screenW, screenH);
+
+            // Input
+            inp = new Input(pp, MainTarget);
+
+            //_player = new Player(Content.Load<Model>("placeholder_character2"), Vector3.Zero, 1.0f, inp, _camera);
+            _camera = new Camera(gpu, Vector3.Down, inp);
+            
+            Window.Title = "HAMMERED";
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            playerTex = Content.Load<Texture2D>("Temp");
+            _player = new Player(Content.Load<Model>("character_test"), Vector3.Zero, 1.5f, inp, _camera, playerTex);
+            _ground = new WorldObject(Content.Load<Model>("dummy_floor"), new Vector3(0, 1f, 0), 5f, inp, _camera, playerTex);
+
+
+            gameObjects = new List<GameObject> { _player, _ground };
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            inp.Update();
+            if (inp.back_down || inp.KeyDown(Keys.Escape)) Exit();
+            //if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            //    Exit();
+
+            foreach (GameObject gameObject in gameObjects)
+            {
+                gameObject.Update(gameTime);
+            }
+            _camera.UpdateCamera();
+
+            base.Update(gameTime);
+        }
+
+        void Set3DStates()
+        {
+            gpu.BlendState = BlendState.NonPremultiplied;
+            gpu.DepthStencilState = DepthStencilState.Default;
+            if (gpu.RasterizerState.CullMode == CullMode.None)
+            {
+                RasterizerState rs = new RasterizerState { CullMode = CullMode.CullCounterClockwiseFace };
+                gpu.RasterizerState = rs;
+            }
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            gpu.SetRenderTarget(MainTarget);
+            gpu.Clear(ClearOptions.Target | ClearOptions.DepthBuffer, Color.AliceBlue, 1.0f, 0);
+            Set3DStates();
+            //gpu.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+            // Render Scene objects
+            foreach (GameObject gameObject in gameObjects)
+            {
+                gameObject.Draw(_camera.view, _camera.proj);
+            }
+
+            // Draw MainTarget to BackBuffer
+            gpu.SetRenderTarget(null);
+            _spriteBatch.Begin(SpriteSortMode.Deferred, BlendState.Opaque, SamplerState.LinearWrap, DepthStencilState.None, RasterizerState.CullNone);
+            _spriteBatch.Draw(MainTarget, desktopRect, Color.White);
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
