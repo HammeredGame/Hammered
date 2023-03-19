af196f402a9570f026c494e2c94f34a29f8f296e
Yuto Takano <takanoy@student.ethz.ch>
2023-03-19 01:16:39 +0100

    Add small documentation for IImGui.cs before I forget

--- src/HammeredGame/HammeredGame/lib/ImMonoGame/ImGui/IImGui.cs
+++ src/HammeredGame/HammeredGame/lib/ImMonoGame/ImGui/IImGui.cs
@@ -1,7 +1,17 @@
 ﻿namespace ImMonoGame.Thing
 {
+    /// <summary>
+    /// Interface for anything that supports an ImGui UI with the UI() method.
+    /// </summary>
     public interface IImGui
     {
+        /// <summary>
+        /// Implement this method to render ImGui UIs. You can optionally wrap the
+        /// declaration in ImGui.Begin() and ImGui.End(), otherwise it will use the
+        /// default window title "Debug" and default styles.
+        /// <para />
+        /// Call this method in the MonoGame Draw() cycle.
+        /// </summary>
         void UI();
     }
 }
