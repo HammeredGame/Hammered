d851d34e9e7f7a55f4cda07446def5feb0be4345
Siddharth Menon <siddharth.krishnaleela@gmail.com>
2023-03-23 17:37:15 +0100

    Fixed another collision issue + Added text to screen when key picked up

--- src/HammeredGame/HammeredGame/Classes/GameObjects/EnvironmentObjects/ObstacleObjs/UnbreakableObstacles/ImmovableObstacle.cs
+++ src/HammeredGame/HammeredGame/Classes/GameObjects/EnvironmentObjects/ObstacleObjs/UnbreakableObstacles/ImmovableObstacle.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,15 @@ using System.Threading.Tasks;
 
 namespace HammeredGame.Classes.GameObjects.EnvironmentObjects.ObstacleObjs.UnbreakableObstacles
 {
-    internal class ImmovableObstacle
+    public class ImmovableObstacle : UnbreakableObstacle
     {
+        // Any Unbreakable Obstacle specific variables go here
+
+        public ImmovableObstacle(Model model, Vector3 pos, float scale, Camera cam, Texture2D t)
+            : base(model, pos, scale, cam, t)
+        {
+        }
     }
 }
+
+
