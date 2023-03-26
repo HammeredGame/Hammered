d26af05a03bb839efb64a491e46eb93b1d4fbfeb
Siddharth Menon <siddharth.krishnaleela@gmail.com>
2023-03-26 12:56:20 +0200

    ObstacleObject and all of its subclasses documented

--- src/HammeredGame/HammeredGame/Game/GameObjects/EnvironmentObjects/ObstacleObjs/BreakableObstacle.cs
+++ src/HammeredGame/HammeredGame/Game/GameObjects/EnvironmentObjects/ObstacleObjs/BreakableObstacle.cs
@@ -8,6 +8,20 @@ using System.Threading.Tasks;
 
 namespace HammeredGame.Game.GameObjects.EnvironmentObjects.ObstacleObjs
 {
+    /// <summary>
+    /// The <c>BreakableObstacle</c> class handles any properties and interactions common to all
+    /// Breakable obstacles (those that will not be rendered on screen after interaction with the hammer) 
+    /// within the game world.
+    /// <para />
+    /// 
+    /// </summary>
+    /// 
+    /// <remarks>
+    /// <para />
+    /// REMINDER (class tree): GameObject -> EnvironmentObject -> ObstacleObject -> BreakableObstacle
+    /// <para />
+    /// </remarks>
+    
     public class BreakableObstacle : ObstacleObject
     {
         // Any Obstacle specific variables go here
@@ -23,6 +37,10 @@ namespace HammeredGame.Game.GameObjects.EnvironmentObjects.ObstacleObjs
 
         public override void TouchingHammer(Hammer hammer)
         {
+            // If the hammer is enroute (i.e in it's destructive state), 
+            // this obstacle's visible boolean will be set to false - which indicates
+            // that this object should no longer be drawn on screen, as well as not be 
+            // considered for future collisions.
             if (hammer.IsEnroute())
                 Visible = false;
             //HammeredGame.activeLevelObstacles.Remove(this);
--- src/HammeredGame/HammeredGame/Game/GameObjects/EnvironmentObjects/ObstacleObjs/UnbreakableObstacles/MovableObstacles/Tree.cs
+++ src/HammeredGame/HammeredGame/Game/GameObjects/EnvironmentObjects/ObstacleObjs/UnbreakableObstacles/MovableObstacles/Tree.cs
@@ -9,6 +9,39 @@ using static HammeredGame.Game.GameObjects.Player;
 
 namespace HammeredGame.Game.GameObjects.EnvironmentObjects.ObstacleObjs.UnbreakableObstacles.MovableObstacles
 {
+    /// <summary>
+    /// The <c>Tree</c> class is a movable obstacle within the game world, contextually
+    /// reacting to the hammer and player interactions. 
+    /// <para />
+    /// Trees have a <code>treeFallen</code> property specific to it, which keeps track of the current
+    /// state of the tree. 
+    /// <para />
+    /// Specifically, if the tree has not fallen (<code>treeFallen == false</code>):
+    ///     --- the player will be fully blocked by the tree
+    ///     --- the hammer (in the <code>Enroute</code> state will:
+    ///         >>> set the tree to a fallen state
+    ///         >>> Rotate the tree to represent it having fallen in the direction of the hammer movement
+    /// <para />
+    /// If the tree as already fallen (<code>treeFallen == true</code>):
+    ///     --- push the player vertically (set the player's Y component) up a little, 
+    ///         if the player collides with the tree
+    ///     --- set the player back to ground level, if the player does not collide with the tree anymore
+    /// </summary>
+    /// 
+    /// <remarks>
+    /// <para />
+    /// REMINDER (class tree): GameObject -> EnvironmentObject -> ObstacleObject -> UnbreakableObstacle
+    ///                         -> MovableObstacle
+    /// <para />
+    /// The current implementation of the tree's interaction with the player after falling is defined as 
+    /// setting the player's Y position to the max Y value of the tree's bounding box. This works alright
+    /// for a flat level, but this will have undesired effects when the puzzles have any kind of 
+    /// elevation introduced. 
+    /// <para />
+    /// TODO: Implement a better way to handle adjusting the player's position, when traversing the 
+    /// tree surface.
+    /// </remarks>
+
     public class Tree : MovableObstacle
     {
         // Any Unbreakable Obstacle specific variables go here
@@ -34,9 +67,6 @@ namespace HammeredGame.Game.GameObjects.EnvironmentObjects.ObstacleObjs.Unbreaka
                 this.Rotation *= Quaternion.CreateFromAxisAngle(Vector3.Cross(Vector3.Up, fallDirection), MathHelper.ToRadians(90));
                 //this.position += new Vector3(0.0f, 20.0f, 0.0f);
                 //System.Diagnostics.Debug.WriteLine(Vector3.UnitZ);
-
-                //System.Diagnostics.Debug.WriteLine(Vector3.Cross(Vector3.Up, fallDirection));
-                //this.additionalTransformation = Matrix.CreateTranslation(Vector3.Zero) * rotationMatrix; // * Matrix.CreateTranslation(this.position);
             }
         }
 
