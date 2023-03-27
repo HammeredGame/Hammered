1cb06732a8c4caf04038575706bbe382ffe53d48
Siddharth Menon <siddharth.krishnaleela@gmail.com>
2023-03-27 14:14:10 +0200

    Added DecorObject class to allow adding decorative elements to our scenes

---
+++ src/HammeredGame/HammeredGame/Game/GameObjects/EnvironmentObjects/DecorObject.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HammeredGame.Game.GameObjects.EnvironmentObjects
+{
+    /// <summary>
+    /// The <c>DecorObject</c> class refers to any decorative element of the environment,
+    /// which will not block the player's movement.
+    /// </summary>
+
+    /// <remarks>
+    /// <para>
+    /// REMINDER (class tree): <see cref="GameObject "/> ---> <see cref="EnvironmentObject "/> 
+    ///                         ---> <see cref="DecorObject "/>
+    /// </para>
+    /// <para>
+    /// TODO: Either we do nothing when colliding with these objects (as it is now), or
+    /// we trigger some sort of animation (like moving grass, etc.)
+    /// </para>
+    /// </remarks>
+    class DecorObject : EnvironmentObject
+    {
+        // Any Interactable specific variables go here
+
+        public DecorObject(Model model, Vector3 pos, float scale, Texture2D t) : base(model, pos, scale, t)
+        {
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            // Do nothing (for now)
+        }
+    }
+}
