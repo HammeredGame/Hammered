41b597d247150a0a89d727e33c5da56efacbff2a
Siddharth Menon <siddharth.krishnaleela@gmail.com>
2023-03-27 12:30:53 +0200

    Slowed down the hammer to ensure it doesn't miss collision frames in the beginning of its return trajectory

--- src/HammeredGame/HammeredGame/Game/GameObjects/Hammer.cs
+++ src/HammeredGame/HammeredGame/Game/GameObjects/Hammer.cs
@@ -43,7 +43,7 @@ namespace HammeredGame.Game.GameObjects
         }
 
         // Hammer specific variables
-        private float hammerSpeed = 0.3f;
+        private float hammerSpeed = 0.2f;
         private HammerState hammerState;
 
         public Vector3 OldPosition { get; private set; }
