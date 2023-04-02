315305bbeffba4fdcad7f3d7cc146cf9ee0d6bcc
kstavratis <kstavratis@student.ethz.ch>
2023-04-02 18:05:01 +0200

    First implementation of the A* algorithm (*not* path planning)
    Things to improve:
    1) Better documentation
    2) Hopefully find a way to do comparisons without the use of the "ID" field, but with the use of the memory position of the vertices instead.

---
+++ src/HammeredGame/HammeredGame/Game/PathPlanning/GraphComponents/Vertex.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HammeredGame.Game.PathPlanning.GraphComponents
+{
+    /// <summary>
+    /// The <c>Vertex</c> class represents a single vertex in a graph.
+    /// A vertex may optionally have:
+    ///     - an ID number (for easier identification in other parts of the code)
+    ///     - directional edges, which connect it to other <c>Vertex</c> instances.
+    /// </summary>
+    public class Vertex : IComparable
+    {
+        public int ID { get; }
+        public double TraveledDistance { get; set; } = double.PositiveInfinity;
+        public double HeuristicValue { get; set; } = double.PositiveInfinity;
+        /// <value>
+        /// The elements "Edges" will be only accessed sequentally during the execution of the A* algorithm.
+        /// Therefore a <c>LinkedList</c> data structure will suffice.
+        /// </value>
+        public LinkedList<Edge> Edges { get; set; }
+
+        public Vertex Origin = null;
+        public Vertex(int id, double traveled, double heuristic)
+        {
+            ID = id;
+            TraveledDistance = traveled;
+            HeuristicValue = heuristic;
+        }
+
+        public Vertex(int id, double traveled, double heuristic, LinkedList<Edge> edges) : this(id, traveled, heuristic)
+        {
+            Edges = edges; 
+        }
+
+        public int CompareTo(object incomingObject)
+        {
+            Vertex incomingVertex = incomingObject as Vertex;
+            return this.ID.CompareTo(incomingVertex.ID);
+        }
+    }
+}
