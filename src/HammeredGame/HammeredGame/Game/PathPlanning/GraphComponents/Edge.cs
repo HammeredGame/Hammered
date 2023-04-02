1cd31f80cc145a680151b064180e7dc609c4ef4b
kstavratis <kstavratis@student.ethz.ch>
2023-04-02 18:05:01 +0200

    First implementation of the A* algorithm (*not* path planning)
    Things to improve:
    1) Better documentation
    2) Hopefully find a way to do comparisons without the use of the "ID" field, but with the use of the memory position of the vertices instead.

---
+++ src/HammeredGame/HammeredGame/Game/PathPlanning/GraphComponents/Edge.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HammeredGame.Game.PathPlanning.Graph
+{
+    public class Edge
+    {
+        double Weight { get; set; } = 0;
+        // Consider storing the source vertex as well.
+        //Vertex SourceVertex { get; set; };
+        Vertex TargetVertex { get; set; } = null;
+        public Edge(double weight, Vertex target) 
+        {
+            Weight = weight;
+            TargetVertex = target;
+        }
+    }
+}
