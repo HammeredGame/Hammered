c41a334852d542c8016a78862446ff6b1a6e1560
Yuto Takano <takanoy@student.ethz.ch>
2023-04-05 16:42:06 +0200

    Introduce a Scene class for abstracting the various scene descriptions

---
+++ src/HammeredGame/HammeredGame/Game/Scene.cs
@@ -0,0 +1,93 @@
+﻿using HammeredGame.Core;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HammeredGame.Game
+{
+
+    /// <summary>
+    /// A Scene Represents one 3D scene. It may or may not have a puzzle or a couple within it, but
+    /// there are always game objects placed around it. A scene definition class will define how the
+    /// scene looks, or how it plays out for the player, including any triggers, scripts, or dialogs.
+    /// </summary>
+    public abstract class Scene
+    {
+
+        /// <summary>
+        /// The camera in the scene.
+        /// </summary>
+        public Camera Camera { get; private set; }
+
+        /// <summary>
+        /// The objects loaded in the scene, keyed by unique identifier strings.
+        /// </summary>
+        public Dictionary<string, GameObject> GameObjects = new();
+
+        public List<GameObject> GameObjectsList
+        {
+            get { return GameObjects.Values.ToList(); }
+        }
+
+        /// <summary>
+        /// Create a new object in the scene
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="name">The unique identifier for the object</param>
+        /// <param name="arguments">The arguments to the constructor of the game object</param>
+        /// <returns></returns>
+        public T Create<T>(string name, params dynamic[] arguments) where T : GameObject
+        {
+            T i = (T)Activator.CreateInstance(typeof(T), args: arguments);
+            GameObjects.Add(name, i);
+            return i;
+        }
+
+        /// <summary>
+        /// Find the object in the scene by a unique identifier. This approaches O(1) since it's a
+        /// dictionary lookup.
+        /// </summary>
+        /// <typeparam name="T">The type of the game object to retrieve and cast</typeparam>
+        /// <param name="name">The unique identifier to find</param>
+        /// <returns>Null if not found, otherwise the game object</returns>
+        public T Get<T>(string name) where T : GameObject
+        {
+            if (GameObjects.TryGetValue(name, out GameObject i))
+            {
+                return (T)i;
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// Remove an object in the scene by a unique identifier.
+        /// </summary>
+        /// <param name="name">The unique identifier to find</param>
+        /// <returns>Whether the removal was successful or not</returns>
+        public bool Remove(string name)
+        {
+            return GameObjects.Remove(name);
+        }
+
+        /// <summary>
+        /// Removes all objects from the scene.
+        /// </summary>
+        public void Clear()
+        {
+            GameObjects.Clear();
+        }
+
+        public void CreateFromXML(GameServices services, string fileName)
+        {
+            XMLLevelLoader ll = new(fileName);
+            Camera = ll.GetCamera(services.GetService<GraphicsDevice>(), services.GetService<Input>());
+            int counter = 0;
+            foreach (GameObject go in ll.GetGameObjects(services, Camera))
+            {
+                GameObjects.Add(counter.ToString(), go);
+                counter++;
+            }
+        }
+    }
+}
---
+++ src/HammeredGame/HammeredGame/Game/Scenes/Island1/TwoIslandPuzzle.cs
@@ -0,0 +1,18 @@
+﻿using HammeredGame.Core;
+using HammeredGame.Game.GameObjects;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace HammeredGame.Game.Scenes.Island1
+{
+    internal class TwoIslandPuzzle : Scene
+    {
+        public TwoIslandPuzzle(GameServices services)
+        {
+            CreateFromXML(services, $"SceneDescriptions/Island1/TwoIslandPuzzle.xml");
+            //Create<Player>("player", services, content.Load<Model>("character-colored"), null, Vector3.Zero, Quaternion.Identity, 0.3f);
+            //Create<Hammer>("hammer", services, content.Load<Model>("temp_hammer2"), null, Vector3.Zero, Quaternion.Identity, 0.3f);
+        }
+    }
+}
