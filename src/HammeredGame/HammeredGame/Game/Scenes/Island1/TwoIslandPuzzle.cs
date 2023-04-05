f77f7f311b25592c3eb52baa4ea7b82dd7e81ab9
Yuto Takano <takanoy@student.ethz.ch>
2023-04-06 01:31:17 +0200

    Implement object creation from debug UI

--- src/HammeredGame/HammeredGame/Game/Scenes/Island1/TwoIslandPuzzle.cs
+++ src/HammeredGame/HammeredGame/Game/Scenes/Island1/TwoIslandPuzzle.cs
@@ -11,24 +11,28 @@ namespace HammeredGame.Game.Scenes.Island1
 {
     internal class TwoIslandPuzzle : Scene
     {
-        public TwoIslandPuzzle(GameServices services)
+        public TwoIslandPuzzle(GameServices services) : base(services)
         {
             CreateFromXML(services, $"SceneDescriptions/Island1/TwoIslandPuzzle.xml");
+
             Get<Player>("player1").SetActiveCamera(Camera);
+
             Get<Hammer>("hammer").SetOwnerPlayer(Get<Player>("player1"));
 
             Get<Door>("door_goal").SetIsGoal(true);
-            Get<PressurePlate>("pressureplate").SetTriggerObject(Get<Door>("door_pp"));
-            Get<Key>("key").SetCorrespondingDoor(Get<Door>("door_goal"));
 
-            //Create<Player>("player", services, content.Load<Model>("character-colored"), null, Vector3.Zero, Quaternion.Identity, 0.3f);
-            //Create<Hammer>("hammer", services, content.Load<Model>("temp_hammer2"), null, Vector3.Zero, Quaternion.Identity, 0.3f);
+            Get<PressurePlate>("pressureplate").SetTriggerObject(Get<Door>("door_pp"));
+
+            Get<Key>("key").SetCorrespondingDoor(Get<Door>("door_goal"));
 
             // Get<Player>("player").OnMove += async _ => {
             //     System.Diagnostics.Debug.WriteLine("a");
             //     services.GetService<ScriptUtils>.WaitSeconds(5);
             //     System.Diagnostics.Debug.WriteLine("written after 5 seconds of player movement");
             // };
+
+            //Create<Player>("player", services, content.Load<Model>("character-colored"), null, Vector3.Zero, Quaternion.Identity, 0.3f);
+            //Create<Hammer>("hammer", services, content.Load<Model>("temp_hammer2"), null, Vector3.Zero, Quaternion.Identity, 0.3f);
         }
     }
 }
