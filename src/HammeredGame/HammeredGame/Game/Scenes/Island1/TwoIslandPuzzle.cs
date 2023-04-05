ef41c8b173a9336322f8de2bb7d81371e27de0e5
Yuto Takano <takanoy@student.ethz.ch>
2023-04-05 22:07:04 +0200

    Add a commented example of scripts in TwoIslandPuzzle

--- src/HammeredGame/HammeredGame/Game/Scenes/Island1/TwoIslandPuzzle.cs
+++ src/HammeredGame/HammeredGame/Game/Scenes/Island1/TwoIslandPuzzle.cs
@@ -13,6 +13,12 @@ namespace HammeredGame.Game.Scenes.Island1
             CreateFromXML(services, $"SceneDescriptions/Island1/TwoIslandPuzzle.xml");
             //Create<Player>("player", services, content.Load<Model>("character-colored"), null, Vector3.Zero, Quaternion.Identity, 0.3f);
             //Create<Hammer>("hammer", services, content.Load<Model>("temp_hammer2"), null, Vector3.Zero, Quaternion.Identity, 0.3f);
+
+            // Get<Player>("player").OnMove += async _ => {
+            //     System.Diagnostics.Debug.WriteLine("a");
+            //     services.GetService<ScriptUtils>.WaitSeconds(5);
+            //     System.Diagnostics.Debug.WriteLine("written after 5 seconds of player movement");
+            // };
         }
     }
 }
