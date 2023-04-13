b7639f46f0103479857915cb828cc9bc8f31c1de
Siddharth Menon <siddharth.krishnaleela@gmail.com>
2023-04-13 23:16:49 +0200

    further commenting for Laser/MoveBlock/Tree.cs

--- src/HammeredGame/HammeredGame/Game/GameObjects/EnvironmentObjects/ObstacleObjs/UnbreakableObstacles/MovableObstacles/MoveBlock.cs
+++ src/HammeredGame/HammeredGame/Game/GameObjects/EnvironmentObjects/ObstacleObjs/UnbreakableObstacles/MovableObstacles/MoveBlock.cs
@@ -72,7 +72,13 @@ namespace HammeredGame.Game.GameObjects.EnvironmentObjects.ObstacleObjs.Unbreaka
                 if (other.Tag is Player)
                 {
                     var player = other.Tag as Player;
-                    player.Entity.Position = new BEPUutilities.Vector3(player.Entity.Position.X, player.Entity.Position.Y + (this.Entity as Box).Width + 1.0f, player.Entity.Position.Z);
+                    float maxY = player.Entity.Position.Y;
+                    foreach (var contact in pair.Contacts)
+                    {
+                        BEPUutilities.Vector3 pointOfContact = contact.Contact.Position;
+                        maxY = Math.Max(maxY, pointOfContact.Y);
+                    }
+                    player.Entity.Position = new BEPUutilities.Vector3(player.Entity.Position.X, maxY + (this.Entity as Box).Width, player.Entity.Position.Z);
                 }
 
                 return;
