8f6da5d7793b84eed89a4b2b5b51bc88d4ad4902
Yuto Takano <takanoy@student.ethz.ch>
2023-04-14 22:36:06 +0200

    Implement showing control prompts keyed by action and input type

--- src/HammeredGame/HammeredGame/Game/ControlPromptsScreen.cs
+++ src/HammeredGame/HammeredGame/Game/ControlPromptsScreen.cs
@@ -1,16 +1,14 @@
 ﻿using FontStashSharp;
 using HammeredGame.Core;
 using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
-using Myra.Graphics2D.Brushes;
+using Myra;
 using Myra.Graphics2D;
+using Myra.Graphics2D.TextureAtlases;
 using Myra.Graphics2D.UI;
 using System;
 using System.Collections.Generic;
-using Myra.Assets;
-using Myra;
-using Myra.Graphics2D.TextureAtlases;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace HammeredGame.Game
 {
@@ -18,10 +16,30 @@ namespace HammeredGame.Game
     {
         private Desktop desktop;
 
-        private Dictionary<CancellationToken, VerticalStackPanel> shownControls = new();
+        private Dictionary<CancellationToken, List<string>> shownControls = new();
         private HorizontalStackPanel controlsPanel;
-        TextureRegionAtlas controlsAtlas;
-        FontSystem barlowFontSystem;
+        private string inputType = "keyboard"; // todo: use enum
+        private Dictionary<string, TextureRegionAtlas> controlsAtlas = new();
+        private FontSystem barlowFontSystem;
+
+        private Dictionary<string, Dictionary<string, string>> controlImageMapping = new()
+        {
+            {
+                // TODO: abstract possible input actions with an enum instead of string
+                "Move", new()
+                {
+                    { "xbox", "XboxSeriesX_Left_Stick" },
+                    { "keyboard", "W_Key_Dark" }
+                }
+            },
+            {
+                "Summon Hammer", new()
+                {
+                    { "xbox", "XboxSeriesX_A" },
+                    { "keyboard", "Space_Key_Dark" }
+                }
+            }
+        };
 
         public ControlPromptsScreen()
         {
@@ -29,44 +47,36 @@ namespace HammeredGame.Game
             PassesFocusThrough = true;
         }
 
-        public void ShowPromptsFor(List<string> controls, CancellationToken stopToken)
+        public void ShowPromptsFor(List<string> actions, CancellationToken stopToken)
         {
-            foreach (string control in controls)
-            {
-                var image = new Image
-                {
-                    Renderable = controlsAtlas[control]
-                };
-
-                var label = new Label
-                {
-                    Text = "test",
-                    Font = barlowFontSystem.GetFont(30)
-                };
-
-                var singleControlLayout = new VerticalStackPanel();
-                singleControlLayout.AddChild(image);
-                singleControlLayout.AddChild(label);
-
-                System.Diagnostics.Debug.WriteLine("called here" + control);
-                controlsPanel.AddChild(singleControlLayout);
-                shownControls.Add(stopToken, singleControlLayout);
-            }
+            shownControls.Add(stopToken, actions);
         }
 
         public override void LoadContent()
         {
             base.LoadContent();
 
+            int tenthPercentageHeight = ScreenManager.GraphicsDevice.Viewport.Height / 10;
+
+            if (GameServices.GetService<Input>().GamePadState.IsConnected)
+            {
+                inputType = "xbox";
+            }
+
             // Myra uses its own asset manager. The default one uses a File stream based
             // implementation that reads from the directory of the currently executing assembly.
-            controlsAtlas = MyraEnvironment.DefaultAssetManager.Load<TextureRegionAtlas>("Content/ControlPrompts/controls_atlas_xbox.xmat");
+            controlsAtlas[inputType] = MyraEnvironment.DefaultAssetManager.Load<TextureRegionAtlas>("Content/ControlPrompts/controls_atlas_" + inputType + ".xmat");
 
             byte[] barlowTtfData = System.IO.File.ReadAllBytes("Content/Barlow-Medium.ttf");
             barlowFontSystem = new FontSystem();
             barlowFontSystem.AddFont(barlowTtfData);
 
-            controlsPanel = new HorizontalStackPanel();
+            controlsPanel = new HorizontalStackPanel()
+            {
+                VerticalAlignment = VerticalAlignment.Bottom,
+                HorizontalAlignment = HorizontalAlignment.Center,
+                Margin = new Thickness(0, 0, 0, tenthPercentageHeight)
+            };
 
             // Add it to the desktop
             desktop = new();
@@ -77,17 +87,75 @@ namespace HammeredGame.Game
         {
             base.Update(gameTime);
 
+            int tenthPercentageHeight = ScreenManager.GraphicsDevice.Viewport.Height / 10;
+
             desktop.UpdateInput();
-            // Remove any control that should no longer be shown
-            // TODO: check if this causes an exception by modifying the dictionary while looping
+            controlsPanel.Widgets.Clear();
+
+            // Create the layout for currently actively shown keys, for the active input type
             foreach (CancellationToken token in shownControls.Keys)
             {
+                // If a cancellation is requested, remove it
                 if (token.IsCancellationRequested)
                 {
-                    controlsPanel.RemoveChild(shownControls[token]);
                     shownControls.Remove(token);
                 }
+                else
+                {
+                    // Loop over each action to be shown with this cancellation token, and add it to the UI
+                    foreach (string action in shownControls[token])
+                    {
+                        var image = new Image
+                        {
+                            // Choose the image suited for the current input type
+                            Renderable = controlsAtlas[inputType][controlImageMapping[action][inputType]],
+                            Opacity = 0.5f,
+                            HorizontalAlignment = HorizontalAlignment.Center,
+                            MaxHeight = tenthPercentageHeight
+                        };
+
+                        var label = new Label
+                        {
+                            // Dark purple
+                            TextColor = new(75, 43, 58),
+                            Text = action,
+                            Font = barlowFontSystem.GetFont(tenthPercentageHeight * 0.5f),
+                            HorizontalAlignment = HorizontalAlignment.Center
+                        };
+
+                        var singleControlLayout = new VerticalStackPanel()
+                        {
+                            HorizontalAlignment = HorizontalAlignment.Center,
+                            Margin = new Thickness(100, 0)
+                        };
+                        singleControlLayout.AddChild(image);
+                        singleControlLayout.AddChild(label);
+
+                        controlsPanel.AddChild(singleControlLayout);
+                    }
+                }
             }
+
+            // Load texture atlas when input type changed. This is IO heavy so do it asynchronously,
+            // and only once for any input type. TODO: abstract away the constant strings somehow.
+            if (GameServices.GetService<Input>().GamePadState.IsConnected && !controlsAtlas.ContainsKey("xbox"))
+            {
+                new Task(() =>
+                {
+                    // Don't set inputType until assets are loaded, since an Update() might use it before
+                    controlsAtlas["xbox"] = MyraEnvironment.DefaultAssetManager.Load<TextureRegionAtlas>("Content/ControlPrompts/controls_atlas_xbox.xmat");
+                    inputType = "xbox";
+                }).Start();
+            } else if (!controlsAtlas.ContainsKey("keyboard"))
+            {
+                new Task(() =>
+                {
+                    // Don't set inputType until assets are loaded, since an Update() might use it before
+                    controlsAtlas["keyboard"] = MyraEnvironment.DefaultAssetManager.Load<TextureRegionAtlas>("Content/ControlPrompts/controls_atlas_keyboard.xmat");
+                    inputType = "keyboard";
+                }).Start();
+            }
+
             desktop.UpdateLayout();
         }
 
