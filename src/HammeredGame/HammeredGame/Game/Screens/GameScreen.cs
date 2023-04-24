e96559685429a843265c910f6b694ed5067d24d6
Yuto Takano <takanoy@student.ethz.ch>
2023-04-25 01:57:22 +0200

    Inline the call to SetupDrawTargets and move to within the renderer
    It doesn't make sense for GameScreen to be concerned with how the destination
    RenderTarget2Ds are set up, so this should be opaquely done with
    DrawScene. Since now it's just a small function call within the same
    class, it has been inlined.
    
    With this approach, we can change to a different render target
    within DrawScene and draw the shadow map too, without GameScreen being
    aware of what the state of the intermediate render targets are.

--- src/HammeredGame/HammeredGame/Game/Screens/GameScreen.cs
+++ src/HammeredGame/HammeredGame/Game/Screens/GameScreen.cs
@@ -212,7 +212,6 @@ namespace HammeredGame.Game.Screens
         {
             base.Draw(gameTime);
 
-            gameRenderer.SetupDrawTargets();
             gameRenderer.DrawScene(currentScene);
             gameRenderer.PostProcess();
             gameRenderer.CopyOutputTo(ScreenManager.MainRenderTarget);
