f89043e28e1e75b1ac98dc201a471298e36de0c6
Yuto Takano <takanoy@student.ethz.ch>
2023-04-25 20:14:46 +0200

    Change Light types to be classes rather than immutable records, add UI to edit sun

--- src/HammeredGame/HammeredGame/Graphics/Light.cs
+++ src/HammeredGame/HammeredGame/Graphics/Light.cs
@@ -5,23 +5,74 @@ namespace HammeredGame.Graphics
 {
     // All lights can be enabled or disabled, have a single color (for simplicity,
     // both the diffuse and specular colours are the same) and have an intensity.
-    public abstract record Light(bool Enabled, Color LightColor, float Intensity);
+    public abstract class Light
+    {
+        public bool Enabled = false;
+        public Color LightColor = Color.White;
+        public float Intensity = 1f;
+        protected Light(bool enabled, Color lightColor, float intensity)
+        {
+            Enabled = enabled;
+            LightColor = lightColor;
+            Intensity = intensity;
+        }
+    }
 
     // A single sun light can exist per scene, and it is essentially an infinite
     // directional light but will also cast shadows.
-    public record SunLight(bool Enabled, Color LightColor, float Intensity, Vector3 Direction) : Light(Enabled, LightColor, Intensity);
+    public class SunLight : Light
+    {
+        public Vector3 Direction;
+        public SunLight(bool enabled, Color lightColor, float intensity, Vector3 direction) : base(enabled, lightColor, intensity)
+        {
+            Direction = direction;
+        }
+    }
 
     // An infinite directional light will affect all objects in the scene purely
     // based on their normal direction.
-    public record InfiniteDirectionalLight(bool Enabled, Color LightColor, float Intensity, Vector3 Direction) : Light(Enabled, LightColor, Intensity);
+    public class InfiniteDirectionalLight : Light
+    {
+        public Vector3 Direction;
+        public InfiniteDirectionalLight(bool enabled, Color lightColor, float intensity, Vector3 direction) : base(enabled, lightColor, intensity)
+        {
+            Direction = direction;
+        }
+    }
+
 
     // An ambient light affects all pixels on the screen, should be used with
     // care since it reduces contrast of the final image.
-    public record AmbientLight(bool Enabled, Color LightColor, float Intensity) : Light(Enabled, LightColor, Intensity);
+    public class AmbientLight : Light
+    {
+        public AmbientLight(bool enabled, Color lightColor, float intensity) : base(enabled, lightColor, intensity) { }
+    }
 
     // Point lights are not implemented yet.
-    public record PointLight(bool Enabled, Color LightColor, Vector3 Position, float Radius, float Intensity) : Light(Enabled, LightColor, Intensity);
+    public class PointLight : Light
+    {
+        public Vector3 Position;
+        public float Radius;
+        public PointLight(bool enabled, Color lightColor, float intensity, Vector3 position, float radius) : base(enabled, lightColor, intensity)
+        {
+            Position = position;
+            Radius = radius;
+        }
+    }
 
     // Spot lights are not implemented yet.
-    public record SpotLight(bool Enabled, Color LightColor, Vector3 Position, Vector3 Direction, float Angle, float Falloff, float Intensity) : Light(Enabled, LightColor, Intensity);
+    public class SpotLight : Light
+    {
+        public Vector3 Position;
+        public Vector3 Direction;
+        public float Angle;
+        public float FallOff;
+        public SpotLight(bool enabled, Color lightColor, float intensity, Vector3 position, Vector3 direction, float angle, float fallOff) : base(enabled, lightColor, intensity)
+        {
+            Position = position;
+            Direction = direction;
+            Angle = angle;
+            FallOff = fallOff;
+        }
+    }
 }
