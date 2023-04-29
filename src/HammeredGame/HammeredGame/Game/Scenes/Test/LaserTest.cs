706da60d0c4301bac3f39a4e899a28a3ac7bb99a
Yuto Takano <takanoy@student.ethz.ch>
2023-04-29 20:12:51 +0200

    Implement laser as a cylindrical billboard and add a test scene for it

---
+++ src/HammeredGame/HammeredGame/Game/Scenes/Test/LaserTest.cs
@@ -0,0 +1,25 @@
+﻿using HammeredGame.Core;
+using HammeredGame.Game.GameObjects;
+using HammeredGame.Game.GameObjects.EnvironmentObjects.ObstacleObjs.UnbreakableObstacles.ImmovableObstacles;
+using HammeredGame.Game.Screens;
+
+namespace HammeredGame.Game.Scenes.Test
+{
+    internal class LaserTest : Scene
+    {
+        public LaserTest(GameServices services, GameScreen screen) : base(services, screen)
+        {
+            CreateFromXML($"Content/SceneDescriptions/Test/LaserTest.xml");
+            OnSceneStart();
+        }
+
+        protected override void OnSceneStart()
+        {
+            Camera.SetFollowTarget(Get<Player>("player1"));
+            Get<Player>("player1").SetActiveCamera(Camera);
+            Get<Hammer>("hammer").SetOwnerPlayer(Get<Player>("player1"));
+            Laser laser1 = Get<Laser>("laser1");
+            laser1.SetLaserDefaultScale(6.0f);
+        }
+    }
+}
