3b45088ae1ceafee2dee5f141eff6f25c736bc82
Yuto Takano <takanoy@student.ethz.ch>
2023-05-05 00:47:44 +0200

    Improved dialogue, tree placement, and script in shore scene

--- src/HammeredGame/HammeredGame/Game/Scenes/Island1/ShoreWakeup.cs
+++ src/HammeredGame/HammeredGame/Game/Scenes/Island1/ShoreWakeup.cs
@@ -1,10 +1,13 @@
-﻿using HammeredGame.Core;
+﻿using BEPUphysics;
+using HammeredGame.Core;
 using HammeredGame.Game.GameObjects;
 using HammeredGame.Game.GameObjects.EmptyGameObjects;
 using HammeredGame.Game.Screens;
+using Pleasing;
 using System;
 using System.Collections.Generic;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace HammeredGame.Game.Scenes.Island1
 {
@@ -30,7 +33,6 @@ namespace HammeredGame.Game.Scenes.Island1
             Get<Hammer>("hammer").DropHammer();
 
             // Show a small dialogue
-            await Services.GetService<ScriptUtils>().WaitSeconds(1);
             await ParentGameScreen.ShowDialogueAndWait("...Where am I?");
 
             // Show movement prompt 1 second after launch
@@ -45,7 +47,11 @@ namespace HammeredGame.Game.Scenes.Island1
 
             // Disable player movement
             Get<Player>("player1").SetActiveCamera(null);
-            await ParentGameScreen.ShowDialogueAndWait("...Huh, what's that hammer over there?");
+            await ParentGameScreen.ShowDialogueAndWait("...Huh, what's that over there?");
+            Camera.SetFollowTarget(Get<Hammer>("hammer"));
+            await ParentGameScreen.ShowDialogueAndWait("Looks like a hammer, and I feel it... calling to me.");
+            await ParentGameScreen.ShowDialogueAndWait("...");
+            await ParentGameScreen.ShowDialogueAndWait("Or me calling it?");
 
             // set hammer owner so we can now summon it
             Get<Hammer>("hammer").SetOwnerPlayer(Get<Player>("player1"));
@@ -54,38 +60,28 @@ namespace HammeredGame.Game.Scenes.Island1
             // THIS IS REQUIRED FOR ALL SCENES!
             Get<Hammer>("hammer").SetSceneUniformGrid(this.Grid);
 
-            // hide movement controls
+            // hide movement controls and show summoning controls
             movementPromptTokenSource.Cancel();
-
-            // show summon controls
             ParentGameScreen.ShowPromptsFor(new List<UserAction>() { UserAction.SummonHammer }, hammerPromptTokenSource.Token);
 
-            // On summon, show cut scene
-            // Keep track of current physics space time and camera distance
-            float normalPhysicsTimeDuration = Space.TimeStepSettings.TimeStepDuration;
-            float normalCameraDistance = Camera.FollowDistance;
-
-            // Start following the hammer very closely, in very slow motion
+            // On summon, show cut scene, hiding the prompts
             await Services.GetService<ScriptUtils>().WaitEvent(Get<Hammer>("hammer"), "OnSummon");
-            Space.TimeStepSettings.TimeStepDuration = normalPhysicsTimeDuration * 0.1f;
-            Camera.FollowDistance = 20f;
+            hammerPromptTokenSource.Cancel();
+
+            // Temporarily take away player controls
             Get<Player>("player1").SetActiveCamera(null);
-            Camera.SetFollowTarget(Get<Hammer>("hammer"));
-
-            // After three seconds, speed up the slow motion a little bit
-            await Services.GetService<ScriptUtils>().WaitSeconds(3);
-            Space.TimeStepSettings.TimeStepDuration = normalPhysicsTimeDuration * 0.6f;
-
-            // Once it reaches the player, reset to normal physics time and camera distance
-            await Services.GetService<ScriptUtils>().WaitEvent(Get<Player>("player1"), "OnHammerRetrieved");
-            Space.TimeStepSettings.TimeStepDuration = normalPhysicsTimeDuration;
-            Camera.FollowDistance = normalCameraDistance;
+            await SummonCutScene();
             Get<Player>("player1").SetActiveCamera(Camera);
             Camera.SetFollowTarget(Get<Player>("player1"));
+
             await ParentGameScreen.ShowDialogueAndWait("Sweet! A hammer I can summon!?");
+            await ParentGameScreen.ShowDialogueAndWait("I've got no memory of why I'm here\nbut hey, I have a... hammer!");
 
             // Now show prompts for dropping too
-            ParentGameScreen.ShowPromptsFor(new List<UserAction>() { UserAction.DropHammer }, hammerPromptTokenSource.Token);
+            hammerPromptTokenSource = new();
+            ParentGameScreen.ShowPromptsFor(
+                new List<UserAction>() { UserAction.DropHammer, UserAction.SummonHammer },
+                hammerPromptTokenSource.Token);
 
             // Once the user has summoned the hammer at least once, make completion trigger
             // available to load next level. This disallows users exploring before interacting and
@@ -93,12 +89,16 @@ namespace HammeredGame.Game.Scenes.Island1
             // Also, make sure the hammer is being carried by the player. If the player does not have the
             // hammer, they will be blocked and not allowed to continue to the next level.
             Get<TriggerObject>("end_trigger").Entity.CollisionInformation.CollisionRules.Personal = BEPUphysics.CollisionRuleManagement.CollisionRule.Normal;
-            Get<TriggerObject>("end_trigger").OnTrigger += (_, _) =>
+            Get<TriggerObject>("end_trigger").OnTrigger += async (_, _) =>
             {
                 if (Get<Hammer>("hammer").IsWithCharacter())
                 {
                     hammerPromptTokenSource.Cancel();
                     ParentGameScreen.InitializeLevel(typeof(TreeTutorial).FullName);
+                }
+                else
+                {
+                    await ParentGameScreen.ShowDialogueAndWait("The hammer might be needed later, let me bring it.");
                 }
             };
 
@@ -106,6 +106,33 @@ namespace HammeredGame.Game.Scenes.Island1
             await Services.GetService<ScriptUtils>().WaitEvent(Get<Player>("player1"), "OnHammerRetrieved");
             await Services.GetService<ScriptUtils>().WaitEvent(Get<Player>("player1"), "OnHammerRetrieved");
             hammerPromptTokenSource.Cancel();
+        }
+
+        /// <summary>
+        /// A cut scene for summoning the hammer. Player controls should be disabled before calling
+        /// this, and enabled after this. It's not included in this function to allow us to tweak
+        /// when the control should be taken or given back.
+        /// </summary>
+        private async Task SummonCutScene()
+        {
+            // Keep track of current physics space time and camera distance
+            float normalPhysicsTimeDuration = Space.TimeStepSettings.TimeStepDuration;
+            float normalCameraDistance = Camera.FollowDistance;
+
+            Camera.FollowDistance = 20f;
+
+            // Interpolate the physics timestep so we go slow-mo and gradually reach normal speed again
+            TweenTimeline physicsTimeInterpolation = Tweening.NewTimeline();
+            physicsTimeInterpolation
+                .AddFloat(Space.TimeStepSettings, nameof(Space.TimeStepSettings.TimeStepDuration))
+                .AddFrame(0, 0f)
+                .AddFrame(4000, normalPhysicsTimeDuration, Easing.Quadratic.In);
+            physicsTimeInterpolation.Start();
+
+            // Once it reaches the player, reset to normal physics time and camera distance
+            await Services.GetService<ScriptUtils>().WaitEvent(Get<Player>("player1"), "OnHammerRetrieved");
+            Space.TimeStepSettings.TimeStepDuration = normalPhysicsTimeDuration;
+            Camera.FollowDistance = normalCameraDistance;
         }
     }
 }
