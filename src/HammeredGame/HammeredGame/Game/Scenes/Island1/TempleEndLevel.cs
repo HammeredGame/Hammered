79e0181c3bebe47d4ce3af83640821ac43569e73
Yuto Takano <takanoy@student.ethz.ch>
2023-05-05 13:16:27 +0200

    Hammer gets sucked into clouds after game clear

--- src/HammeredGame/HammeredGame/Game/Scenes/Island1/TempleEndLevel.cs
+++ src/HammeredGame/HammeredGame/Game/Scenes/Island1/TempleEndLevel.cs
@@ -2,14 +2,16 @@
 using HammeredGame.Game.GameObjects;
 using HammeredGame.Game.GameObjects.EmptyGameObjects;
 using HammeredGame.Game.Screens;
-using System;
-using System.Collections.Generic;
-using System.Threading;
+using HammeredGame.Game.GameObjects.EnvironmentObjects.InteractableObjs.ImmovableInteractables;
+using Microsoft.Xna.Framework;
+using Pleasing;
 
 namespace HammeredGame.Game.Scenes.Island1
 {
     internal class TempleEndLevel : Scene
     {
+        private bool ended = false;
+        
         public TempleEndLevel(GameServices services, GameScreen screen) : base(services, screen)
         {
             CreateFromXML($"Content/SceneDescriptions/Island1/TempleEndLevel_voxel.xml");
@@ -27,6 +29,25 @@ namespace HammeredGame.Game.Scenes.Island1
 
             await ParentGameScreen.ShowDialogueAndWait("A temple with a huge statue of a hammer...\nGee, I wonder which god this is for.");
             await ParentGameScreen.ShowDialogueAndWait("Let's hurry up and drop off the hammer!");
+            
+                
+        }
+
+        public override async void Update(GameTime gameTime, bool screenHasFocus, bool isPaused)
+        {
+            base.Update(gameTime, screenHasFocus, isPaused);
+
+            if (Get<PressurePlate>("temple_pressure_plate").IsActivated() && !ended)
+            {
+                ended = true;
+                Hammer hammer = Get<Hammer>("hammer");
+                hammer.DropHammer();
+                hammer.Entity.Gravity = new BEPUutilities.Vector3(0, 50f, 0);
+                await ParentGameScreen.ShowDialogueAndWait("(The hammer was sucked into the clouds)");
+                await ParentGameScreen.ShowDialogueAndWait("(You hear Thor sounding happy! He'll send you home!");
+                await Services.GetService<ScriptUtils>().WaitSeconds(2);
+                await ParentGameScreen.ShowDialogueAndWait("The End! (for now)");
+            }
         }
     }
 }
