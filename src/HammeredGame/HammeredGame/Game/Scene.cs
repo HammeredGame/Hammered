6c33a59bc59177f72c3c8b15a62bb5f708f20903
Yuto Takano <takanoy@student.ethz.ch>
2023-05-14 18:53:20 +0200

    Keep custom name when duplicating objects

--- src/HammeredGame/HammeredGame/Game/Scene.cs
+++ src/HammeredGame/HammeredGame/Game/Scene.cs
@@ -639,7 +639,7 @@ namespace HammeredGame.Game
                             if (ImGui.MenuItem("Duplicate Object"))
                             {
                                 // Generate a new name for the object
-                                string name = GenerateUniqueNameWithPrefix(gameObject.GetType().Name.ToLower());
+                                string name = GenerateUniqueNameWithPrefix(key.TrimEnd(new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' }));
 
                                 // Copy the entity
                                 Entity entity = null;
