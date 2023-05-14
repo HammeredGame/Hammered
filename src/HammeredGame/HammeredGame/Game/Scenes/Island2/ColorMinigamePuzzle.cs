bfb314dfb4e4e3b7438902ed61a524b7f6472d25
Yuto Takano <takanoy@student.ethz.ch>
2023-05-14 15:06:29 +0200

    Initial file creation for color  minigame puzzle

---
+++ src/HammeredGame/HammeredGame/Game/Scenes/Island2/ColorMinigamePuzzle.cs
@@ -0,0 +1,37 @@
+﻿using BEPUphysics.CollisionRuleManagement;
+using HammeredGame.Core;
+using HammeredGame.Game.GameObjects;
+using HammeredGame.Game.GameObjects.EmptyGameObjects;
+using HammeredGame.Game.GameObjects.EnvironmentObjects.ObstacleObjs.UnbreakableObstacles.ImmovableObstacles;
+using HammeredGame.Game.GameObjects.EnvironmentObjects.ObstacleObjs.UnbreakableObstacles.MovableObstacles;
+using HammeredGame.Game.Screens;
+using System;
+using System.Threading.Tasks;
+
+namespace HammeredGame.Game.Scenes.Island2
+{
+    internal class ColorMinigamePuzzle : Scene
+    {
+        public ColorMinigamePuzzle(GameServices services, GameScreen screen) : base(services, screen)
+        { }
+        protected override async Task LoadSceneContent(IProgress<int> progress)
+        {
+            await base.LoadSceneContent(progress);
+            await CreateFromXML($"Content/SceneDescriptions/Island2/ColorMinigamePuzzle.xml", progress);
+        }
+
+        protected override void OnSceneStart()
+        {
+            Camera.SetFollowTarget(Get<Player>("player1"));
+            Get<Player>("player1").SetActiveCamera(Camera);
+            Get<Hammer>("hammer").SetOwnerPlayer(Get<Player>("player1"));
+            // Allow <c>Hammer</c> instance to have access to the grid of the scene for the path planning.
+            // THIS IS REQUIRED FOR ALL SCENES!
+            Get<Hammer>("hammer").SetSceneUniformGrid(this.Grid);
+
+            // Set laser to desired length within level
+            Laser laser1 = Get<Laser>("laser1");
+            laser1.SetLaserDefaultScale(2.0f);
+        }
+    }
+}
