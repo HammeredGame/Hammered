0f3575cddefcaf5ebae28ec765679916b8aad483
Audrey Leong <audreyleong@Audreys-MacBook-Air-2.local>
2023-05-15 10:40:32 +0200

    additional dialogue changes

--- src/HammeredGame/HammeredGame/Game/GameObjects/EnvironmentObjects/ObstacleObjs/UnbreakableObstacles/ImmovableObstacles/Laser.cs
+++ src/HammeredGame/HammeredGame/Game/GameObjects/EnvironmentObjects/ObstacleObjs/UnbreakableObstacles/ImmovableObstacles/Laser.cs
@@ -128,7 +128,7 @@ namespace HammeredGame.Game.GameObjects.EnvironmentObjects.ObstacleObjs.Unbreaka
             if (timeDelay < TimeSpan.Zero)
             {
                 Services.GetService<AudioManager>().Play3DSound("Audio/buzz", false, this.AudioEmitter, laserScale / laserDefaultScale);
-                timeDelay += TimeSpan.FromSeconds(1.2f);
+                timeDelay += TimeSpan.FromSeconds(1.15f);
             }
             
             
