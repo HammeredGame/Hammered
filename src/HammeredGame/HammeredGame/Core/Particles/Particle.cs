2b1747a57498a06b44a682e81d20e2fb8379763d
Yuto Takano <takanoy@student.ethz.ch>
2023-05-22 16:42:46 +0200

    Fix particle size interpolation from exponential decay to linear decay

--- src/HammeredGame/HammeredGame/Core/Particles/Particle.cs
+++ src/HammeredGame/HammeredGame/Core/Particles/Particle.cs
@@ -12,7 +12,13 @@ namespace HammeredGame.Core.Particles
         // The time (in seconds) at which this particle was created.
         public float Time;
 
-        // The current size of the particle (in world units).
+        // The start size of the particle in world units.
+        public float StartSize;
+
+        // The current size of the particle in world units.
         public float Size;
+
+        // The end size of the particle in world units.
+        public float EndSize;
     }
 }
